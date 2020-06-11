aeab3f21e5d4b904f770f42052d17b4e03dbab0f
degski <degski@gmail.com>
2020-06-11 10:12:04 -0500

    threads and prng

--- main.cpp
+++ main.cpp
@@ -21,29 +21,87 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <fcntl.h>
-#include <io.h>
-
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
-#include <cstdio>
 #include <cstdlib>
+#include <string>
 
 #include <array>
-#include <chrono>
+#include <atomic>
+#include <algorithm>
 #include <filesystem>
-#include <fstream>
-#include <iterator>
-#include <list>
-#include <map>
-#include <random>
+
+using namespace fs = std::file_system;
+
+#include <initializer_list>
 #include <sax/iostream.hpp>
-#include <sstream>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <random>
+#include <span>
+#include <stdexcept>
 #include <string>
-#include <string_view>
 #include <thread>
+#include <jthread>
 #include <type_traits>
+#include <utility>
 #include <vector>
+
+/*
+    -fsanitize = address
+
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan_cxx-x86_64.lib
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan-preinit-x86_64.lib
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan-x86_64.lib
+
+    C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\tbb\lib\intel64_win\vc_mt\tbb.lib
+*/
+
+#include <sax/prng_sfc.hpp>
+#include <sax/uniform_int_distribution.hpp>
+
+#if defined( NDEBUG )
+#    define RANDOM 1
+#else
+#    define RANDOM 0
+#endif
+
+namespace ThreadID {
+// Creates a new ID.
+[[nodiscard]] inline int get ( bool ) noexcept {
+    static std::atomic<int> global_id = 0;
+    return global_id++;
+}
+// Returns ID of this thread.
+[[nodiscard]] inline int get ( ) noexcept {
+    static thread_local int thread_local_id = get ( false );
+    return thread_local_id;
+}
+} // namespace ThreadID
+
+namespace Rng {
+// Chris Doty-Humphrey's Small Fast Chaotic Prng.
+[[nodiscard]] inline sax::Rng & generator ( ) noexcept {
+    if constexpr ( RANDOM ) {
+        static thread_local sax::Rng generator ( sax::os_seed ( ), sax::os_seed ( ), sax::os_seed ( ), sax::os_seed ( ) );
+        return generator;
+    }
+    else {
+        static thread_local sax::Rng generator ( sax::fixed_seed ( ) + ThreadID::get ( ) );
+        return generator;
+    }
+}
+} // namespace Rng
+
+#undef RANDOM
+
+sax::Rng & rng = Rng::generator ( );
+
+#include <fcntl.h>
+#include <io.h>
 
 #include <sax/utf8conv.hpp>
 
